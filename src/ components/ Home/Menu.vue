11c9b8161d941ff50c2b5b4e13a4d679f24bdbc7
MaChao01 <34291764+MaChao01@users.noreply.github.com>
2019-06-09 19:33:50 +0800

    Update and rename src/components/Home/Menu.vue to  src/ components/ Home/Menu.vue

---
+++ src/ components/ Home/Menu.vue
@@ -0,0 +1,81 @@
+<!-- html部分 -->
+<template>
+	<div style="height: 100%;">
+		<!-- 菜单 -->
+		<el-menu :default-active='$store.state.defaultActive'>
+			<span v-for="obj in arr">
+				<!-- 没有子菜单 -->
+				<el-menu-item v-show='!obj.children' :index='obj.index' @click='fn(obj)'>
+					<span slot="title">{{obj.text}}</span>
+				</el-menu-item>
+				<!-- 有子菜单 -->
+				<el-submenu v-show='obj.children' :index='obj.index'>
+					<template slot="title">
+					  <span>{{obj.text}}</span>
+					</template>
+					<el-menu-item-group>
+					  <el-menu-item v-for='item in obj.children' :index='item.index' :key='item.text' @click='fn(item)'>{{item.text}}</el-menu-item>
+					</el-menu-item-group>
+				</el-submenu>
+			</span>
+		</el-menu>
+	</div>
+</template>
+<!-- js部分 -->
+<script>
+	export default {
+		name:'',
+		data() {
+			return {
+				arr:[{text:'首页',index:'101',url:'/'},
+				{text:'基本信息',index:'102',children:[
+						 {text:'企业信息',index:'1021',url:"/Test01"},
+						 {text:'人员信息',index:'1022',url:"/userlist"},
+						 {text:'设备类别',index:'1023',url:"/devtypelist"},
+						 {text:'设备信息',index:'1024',url:"/devicelist"},
+						 {text:'流程信息',index:'1025',url:"/flowlist"}
+					 ]},
+					 {text:'生产作业',index:'103',children:[
+						 {text:'故障信息',index:'1031',url:"/faultlist"},
+						 {text:'维修记录',index:'1032',url:"/repairlist"},
+					 	 {text:'报岗',index:'1033',url:"/reportlist"},
+					 	 {text:'中控',index:'1034',url:"/Control"},
+						 {text:'作业信息',index:'1035',url:"/joblist"},
+					]},
+					 {text:'能耗信息',index:'104',children:[
+					 	 {text:'能耗信息',index:'1041',url:"consumelist"},
+					 	 {text:'折标煤系数',index:'1042',url:"standradcoallist"},	
+					 ]},
+					 {text:'统计信息',index:'105',children:[
+					 	 {text:'作业量统计',index:'1051',url:"produce"},
+					 ]},
+					  {text:'用能利率',index:'106',children:[
+					 	 {text:'设备利用率',index:'1061',url:"usage"},
+					 	 {text:'设备完好率',index:'1062',url:"intactRatio"},
+						 {text:'成本核算',index:'1063',url:"cost"},
+						 {text:'作业量',index:'1064',url:"amount"},
+						 {text:'能耗',index:'1065',url:"consume"},
+					 ]},
+					 {text:'用能模型',index:'107',children:[
+					 	 {text:'企业用能模型',index:'1071',url:"energy"},
+						 {text:'企业产销模型',index:'1072',url:"EnergyModel"},
+					 ]},
+				
+			};
+		},
+		methods:{
+			fn(obj){
+				// 路由跳转
+				this.$router.push({path:obj.url});
+				// 添加到tags
+				this.$store.commit('addTag',{name:obj.text,type:'warning',url:obj.url,index:obj.index});
+			}
+		}
+	}
+</script>
+<!-- css部分 -->
+<style>
+.el-menu{
+	background: #F4F4F4;
+}
+</style>
